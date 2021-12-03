947da3817b4da822e8d630156d82f4a9e68fbd45
JoshPrakke <Joshua.Prakke@capco.com>
2021-12-02 20:10:28 -0500

    moved solution one to its own method and removed un needed prints

--- day_2/SubMovement.java
+++ day_2/SubMovement.java
@@ -12,27 +12,28 @@ public class SubMovement {
     public static void main(String[] args) throws FileNotFoundException {
         GetData gd= new GetData();
         String[] input = gd.getArray("./day_2/input.txt");
+
+        challengeOne(input);
+    }
+    public static void challengeOne(String[] input) {
         List<String> movements = new ArrayList<String>();
         int horizontalPos = 0;
         int depthPos = 0;
         int output = 0;
-
-        for(String line : input){
-            for(String s : line.split(" "))
+        for (String line : input) {
+            for (String s : line.split(" "))
                 movements.add(s);
         }
-        for(int i = 0; i<movements.size(); i += 2){
-            if (movements.get(i).equals("forward")){
-                horizontalPos += parseInt(movements.get(i+1));
-            }else if(movements.get(i).equals("down")){
-                depthPos += parseInt(movements.get(i+1));
-            }else if(movements.get(i).equals("up")){
-                depthPos -= parseInt(movements.get(i+1));
+        for (int i = 0; i < movements.size(); i += 2) {
+            if (movements.get(i).equals("forward")) {
+                horizontalPos += parseInt(movements.get(i + 1));
+            } else if (movements.get(i).equals("down")) {
+                depthPos += parseInt(movements.get(i + 1));
+            } else if (movements.get(i).equals("up")) {
+                depthPos -= parseInt(movements.get(i + 1));
             }
-            System.out.println(movements.get(i));
-            System.out.println(movements.get(i+1));
         }
-        output += depthPos*horizontalPos;
+        output += depthPos * horizontalPos;
         System.out.println(output);
     }
 }
