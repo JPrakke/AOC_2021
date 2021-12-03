346ec1a33a15ff7e086b52444c36f7ade9ef5bb3
JoshPrakke <Joshua.Prakke@capco.com>
2021-12-02 20:06:56 -0500

    Finished solution 1 for Day 2

--- day_2/SubMovement.java
+++ day_2/SubMovement.java
@@ -2,11 +2,37 @@ package day_2;
 
 import day_1.GetData;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Integer.parseInt;
 
 public class SubMovement {
+
     public static void main(String[] args) throws FileNotFoundException {
         GetData gd= new GetData();
         String[] input = gd.getArray("./day_2/input.txt");
+        List<String> movements = new ArrayList<String>();
+        int horizontalPos = 0;
+        int depthPos = 0;
+        int output = 0;
 
+        for(String line : input){
+            for(String s : line.split(" "))
+                movements.add(s);
+        }
+        for(int i = 0; i<movements.size(); i += 2){
+            if (movements.get(i).equals("forward")){
+                horizontalPos += parseInt(movements.get(i+1));
+            }else if(movements.get(i).equals("down")){
+                depthPos += parseInt(movements.get(i+1));
+            }else if(movements.get(i).equals("up")){
+                depthPos -= parseInt(movements.get(i+1));
+            }
+            System.out.println(movements.get(i));
+            System.out.println(movements.get(i+1));
+        }
+        output += depthPos*horizontalPos;
+        System.out.println(output);
     }
 }
